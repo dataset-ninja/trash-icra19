b42c8052b9d1299b2acf4fd42a8302685dfbbd9e
peixed351 <peixed351@gmail.com>
2024-04-03 15:30:50 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Trash-ICRA19"
 PROJECT_NAME_FULL: str = "Trash-ICRA19: A Bounding Box Labeled Dataset of Underwater Trash"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
